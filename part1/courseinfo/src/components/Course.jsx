c02edfeaa6a37b80a3f8a045cf8268ba9c77a201
Christine Lin <lin.christine19@gmail.com>
2025-11-06 13:34:54 -0500

    courseinfo exercise 2.5

---
+++ part1/courseinfo/src/components/Course.jsx
@@ -0,0 +1,35 @@
+const Header = ({course}) => <h2>{course}</h2>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part =>
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, curr) => acc + curr.exercises, 0)
+  return (
+    <strong>total of {total} exercises</strong>
+  )
+}
+
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
+}
+
+export default Course
