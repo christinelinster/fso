bef60c81bc1f2fc1106d855b1fbc726e1b52905b
Christine Lin <lin.christine19@gmail.com>
2025-11-05 22:41:33 -0500

    unicafe exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
     setter(count + 1)
   }
 
-
   return (
     <div>
       <h1>give feedback</h1>
@@ -21,6 +20,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {((good) / (good + neutral + bad)) * 100} %</p>
     </div>
   )
 }
