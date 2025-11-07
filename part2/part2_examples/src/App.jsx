ad90ee29cac48ad93b0adaf51cc3e16af69bfb5a
Christine Lin <lin.christine19@gmail.com>
2025-11-07 13:05:35 -0500

    part2 notes example: send data to the server

--- part2/part2_examples/src/App.jsx
+++ part2/part2_examples/src/App.jsx
@@ -25,14 +25,18 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: String(notes.length + 1)
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
