fbc0f3e48eb468429f7193fdb5cb61ed5235bce5
Christine Lin <lin.christine19@gmail.com>
2025-12-08 13:05:53 -0500

    exercise 3.10 deploy phonebook to render

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -7,6 +8,7 @@ morgan.token('content', (req, res) => JSON.stringify(req.body))
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :content'))
+app.use(cors())
 
 let persons = [
     {
